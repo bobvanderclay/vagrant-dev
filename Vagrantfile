39ddeddb3ad3a43888fa8af8a59afc9faac8620e
Bob VanderClay <bob@vanderclay.net>
2014-08-14 09:19:48 -0700

    Added Node, back to static IP.

--- Vagrantfile
+++ Vagrantfile
@@ -26,7 +26,8 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
   # Create a private network, which allows host-only access to the machine
   # using a specific IP.
-  config.vm.network "private_network", type: "dhcp"
+  config.vm.network :private_network, ip: "192.168.57.2"
+  config.vm.hostname = "local.dev"
 
   # Create a public network, which generally matched to bridged network.
   # Bridged networks make the machine appear as another physical device on
--- provision.sh
+++ provision.sh
@@ -1,23 +1,45 @@
+# Install Some PPAs
+
+apt-get install -y software-properties-common
+apt-add-repository ppa:chris-lea/node.js -y
+
+# Update Package Lists
 
 apt-get update
+apt-get upgrade -y
 
 # Install core packages
+
 apt-get install -y \
   git \
   curl \
   build-essential \
+  cmake \
+  python-dev \
   libevent-dev \
   libncurses-dev \
   vim \
   zsh
 
 # Install docker
+
 apt-get install -y docker.io
 ln -sf /usr/bin/docker.io /usr/local/bin/docker
 usermod -aG docker vagrant
 
 # Install tmux 1.9a
+
 mkdir /opt/tmux
 cd /opt/tmux && curl -L -O http://downloads.sourceforge.net/tmux/tmux-1.9a.tar.gz && tar xzf tmux-1.9a.tar.gz
 cd /opt/tmux/tmux-1.9a && ./configure && make && make install
+
+# Install node 
+
+apt-get install -y node.js
+apt-get install -y npm
+
+npm install -g grunt-cli
+npm install -g bower
+npm install -g gulp
+npm install -g express
 
